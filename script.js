a8e56f388326a9b26ea75eece3591abc99a0699f
MATIAZZZZZ <frankdiegomatias4567@gmail.com>
2026-01-01 11:09:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+  window.onload = () => {
+    // Remueve la clase .container del body cuando la página ha cargado
+    // para iniciar las animaciones si están pausadas por defecto
+    const c = setTimeout(() => {
+      document.body.classList.remove("container");
+      clearTimeout(c);
+    }, 1000);
+  };
